d3c8205334014bf456658dcbd43e06bac3838194
eJoy89 <100457697+eJoy89@users.noreply.github.com>
2022-04-07 23:37:13 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,111 @@
+$(document).ready(function(){
+    media();
+    AOS.init();
+
+    function media (){
+        var ww = $(window).width();
+        if(ww > 1400){
+            $('#wrap').fullpage({
+                scrollBar:true
+            });
+
+
+            $('.h-menu li').click(function(){
+                $(this).addClass('active');
+                $(this).siblings().removeClass('active');
+        
+                var result = $(this).attr('data-alt');
+        
+                $('.tabs div').removeClass('active');
+                $('#' + result).addClass('active');
+        
+            });    
+        
+        
+            $('.h-menu li .menu-name').click(function(){
+                $(this).toggleClass('active').parent('.menu-box-h').siblings().children().removeClass('active');
+            });    
+        
+        
+            $('.h-menu .menu-box-h').click(function(){
+                $(this).children('.sub-menu').stop().slideToggle();
+                $(this).siblings().children('.sub-menu').stop().slideUp();
+        
+            });
+             
+        
+            $('#hamburger').click(function(){
+                $('#hamburger').toggleClass('active');
+                $('nav').toggleClass('active');
+            });       
+            
+        
+            $('.check-avaliable').click(function(){
+                $('.black-background').fadeIn();
+            });
+        
+            $('.black-background, .modal-btn button').click(function(e){
+                console.log(e.target);
+        
+                if(e.target == e.currentTarget){
+                    $('.black-background').fadeOut();
+                }
+            });
+
+
+        }else{
+            $('.h-menu li').click(function(){
+                $(this).addClass('active');
+                $(this).siblings().removeClass('active');
+        
+                var result = $(this).attr('data-alt');
+        
+                $('.tabs div').removeClass('active');
+                $('#' + result).addClass('active');
+        
+            });
+        
+        
+        
+            $('.h-menu li .menu-name').click(function(){
+                $(this).toggleClass('active').parent('.menu-box-h').siblings().children().removeClass('active');
+            });
+        
+        
+        
+        
+            $('.h-menu .menu-box-h').click(function(){
+                $(this).children('.sub-menu').stop().slideToggle();
+                $(this).siblings().children('.sub-menu').stop().slideUp();
+        
+            });
+        
+        
+            
+        
+            $('#hamburger').click(function(){
+                $('#hamburger').toggleClass('active');
+                $('nav').toggleClass('active');
+            });
+        
+        
+            
+        
+            $('.check-avaliable').click(function(){
+                $('.black-background').fadeIn();
+            });
+        
+            $('.black-background, .modal-btn button').click(function(e){
+                console.log(e.target);
+        
+                if(e.target == e.currentTarget){
+                    $('.black-background').fadeOut();
+                }
+            });
+        }
+    }
+
+
+    
+
+});
